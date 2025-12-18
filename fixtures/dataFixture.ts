955f4679a2a604fa288d27ef40179bbce1c5180b
Varalakshmidvl <varalakshmi.doddi@gmail.com>
2025-12-18 12:14:03 +0530

    added Jenkins file

--- fixtures/dataFixture.ts
+++ fixtures/dataFixture.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* eslint-disable no-empty-pattern */
 import { expect, test as base } from '@playwright/test';
 import fs from 'fs';
@@ -29,3 +30,35 @@ export const test = base.extend<csvFixture>({
 
 
 export { expect };
+=======
+import { expect, test as base } from '@playwright/test';
+import fs from 'fs';
+import { parse } from 'csv-parse/sync';
+
+//schema/type of reg data fields
+export type RegData = {
+    firstName: string,
+    lastName: string,
+    telephone: string,
+    password: string,
+    subscribeNewsletter: string
+}
+
+type csvFixture = {
+    regData: RegData[];
+}
+
+export const test = base.extend<csvFixture>({
+    regData: async ({ }, use) => {
+        let fileContent = fs.readFileSync('./data/register.csv', 'utf-8');
+        let registerationData: RegData[] = parse(fileContent, {
+            columns: true,
+            skip_empty_lines: true
+        });
+        await use(registerationData);
+    }
+});
+
+
+export { expect };
+>>>>>>> fcb12a2 (Jenkins file for CICD)
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ResultsPage } from '../pages/ResultsPage';
 import { test, expect } from '../fixtures/baseFixtures';
 
@@ -24,3 +25,31 @@ for (const product of searchData) {
 }
 
 
+=======
+import { ResultsPage } from '../pages/ResultsPage';
+import { test, expect } from '../fixtures/baseFixtures'
+
+
+//data provider for product search key and results count
+let searchData = [
+    { searchkey: 'macbook', resultscount: 3 },
+    { searchkey: 'samsung', resultscount: 2 },
+    { searchkey: 'imac', resultscount: 1 },
+    {searchkey: 'canon', resultscount: 1},
+    {searchkey: 'Dummy', resultscount: 0},
+
+];
+
+
+for (let product of searchData) {
+    test(`@search @sanity verify product search ${product.searchkey}`, async ({ homePage }) => {
+
+    let resultsPage: ResultsPage = await homePage.doSearch(product.searchkey);
+    expect (await resultsPage.getSearchResultsCount()).toBe(product.resultscount);
+
+});
+
+}
+
+
+>>>>>>> fcb12a2 (Jenkins file for CICD)
