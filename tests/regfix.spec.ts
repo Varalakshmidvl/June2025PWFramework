c88ea83783ad0fd1222bc4a514f492df1525d9d7
Varalakshmidvl <varalakshmi.doddi@gmail.com>
2025-12-18 14:57:20 +0530

    Manually done conflict resolution and cleanup

--- tests/regfix.spec.ts
+++ tests/regfix.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { test, expect } from '../fixtures/dataFixture';
 import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
@@ -8,8 +7,7 @@ function getRandomEmail() : string{
     return `auto_${randomValue}@nal.com`;
 }
 
-  
-test('Register a user from CSV', async ({ regData, page, baseURL }) => {
+  test('Register a user from CSV', async ({ regData, page, baseURL }) => {
     
     for (const user of regData) {
         const loginPage = new LoginPage(page);
@@ -28,36 +26,3 @@ test('Register a user from CSV', async ({ regData, page, baseURL }) => {
 
 });
 
-
-=======
-import { test, expect } from '../fixtures/dataFixture';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
-
-function getRandomEmail() : string{
-    let randomValue = Math.random().toString(36).substring(2, 9);
-    return `auto_${randomValue}@nal.com`;
-}
-
-  
-test(`Register a user from CSV`, async ({ regData, page, baseURL }) => {
-    
-    for (const user of regData) {
-        let loginPage = new LoginPage(page);
-                await loginPage.goToLoginPage(baseURL);
-                let registerPage: RegisterPage = await loginPage.navigateToRegisterPage();
-                let isUserRegistered: boolean = await registerPage.registerUser(
-                    user.firstName,
-                    user.lastName,
-                    getRandomEmail(),
-                    user.telephone,
-                    user.password, 
-                    user.subscribeNewsletter);
-                expect(isUserRegistered).toBeTruthy();
-    }
-
-
-})
-
-
->>>>>>> fcb12a2 (Jenkins file for CICD)
